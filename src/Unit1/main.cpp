45632e599bdd4ae96daa05d5b0278f8e82882bb7
吉岡真輝 <masaki@masakis-MacBook-Air.local>
2024-02-09 14:04:06 +0900

    some message

--- src/Unit1/main.cpp
+++ src/Unit1/main.cpp
@@ -3,5 +3,15 @@
 int main()
 {
     std::cout << "Hello World\n";
+
+    std::cout << 2020 + 1 << "\n";
+    std::cout << 2020 - 1 << "\n";
+    std::cout << 2020 * 2 << "\n";
+    std::cout << 2020 / 5 << "\n";
+
+    std::cout << (2020 + 5) / 5 << "\n";
+
+    
+
     return 0;
 }
